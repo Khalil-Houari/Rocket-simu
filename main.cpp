785476d1c750e09a526d5c2616f5b11a053b2fec
Houari Khalil <houari.khalil.hadj@gmail.com>
2026-06-18 01:44:07 +0200

    more accurate modeling

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
-#define PI 3.14159265358979323846f
 
 float dt; // will be defined , it is not the time step for RK4 
 bool state = false; // will be used to break the simulation incase of a bug detection or absurd value 
@@ -209,12 +208,21 @@ public:
 };
 
 
-// helper function 
+// helper functions
+
+M3 inertia_cyclindre_emp(float mass, float ext_radius, float int_radius, float height) {
+    M3 inertia;
+    inertia.m[0][0] = mass * (3.0f * (ext_radius * ext_radius + int_radius * int_radius) + height * height) / 12.0f; 
+    inertia.m[1][1] = inertia.m[0][0]; 
+    inertia.m[2][2] =  0.5f * mass * (int_radius * int_radius + ext_radius * ext_radius); // 
+    return inertia;
+}
+
 M3 inertia_cyclindre(float mass, float radius, float height) {
     M3 inertia;
-    inertia.m[0][0] = mass * (3.0f *radius * radius + height * height) / 12.0f; // Ixx = Iyy = (1/12) * m * (3r^2 + h^2)
+    inertia.m[0][0] = mass * (3.0f * (radius * radius ) + height * height) / 12.0f; // Ixx = Iyy = (1/12) * m * (3r^2 + h^2)
     inertia.m[1][1] = inertia.m[0][0]; 
-    inertia.m[2][2] =  0.5f * mass * radius * radius; // Izz = (1/2) * m * r^2
+    inertia.m[2][2] =  0.5f * mass * (radius * radius ); // Izz = (1/2) * m * r^2
     return inertia;
 }
 
@@ -228,23 +236,33 @@ M3 inertia_parallelepiped(float mass, float length, float width, float height) {
 
 M3 inertia_rhead(float density, float radius, float height) {
     M3 inertia;
-    inertia.m[0][0] = PI*density*(32.0f * radius * radius * radius * radius * height/315.0f + 29*radius * radius * height * height * height/105.0f); 
+    inertia.m[0][0] = M_PI*density*(32.0f * radius * radius * radius * radius * height/315.0f + 29*radius * radius * height * height * height/105.0f); 
     inertia.m[1][1] = inertia.m[0][0];
-    inertia.m[2][2] = 0.5f * PI * 128.0f * density * radius * radius * radius * radius * height/315.0f; // 
+    inertia.m[2][2] = 0.5f * M_PI * 128.0f * density * radius * radius * radius * radius * height/315.0f; // 
     return inertia;
 }
     
-M3 steiner(float mass, float d) {
+M3 steiner(float mass, const V3& d) {
     M3 result;
-    result.m[0][0] = mass * d * d; 
-    result.m[1][1] = mass * d * d; 
-    result.m[2][2] = 0.0f;         
+    
+    result.m[0][0] = mass * (d.y * d.y + d.z * d.z); 
+    result.m[0][1] = -mass * d.x * d.y;
+    result.m[0][2] = -mass * d.x * d.z;
+
+    result.m[1][0] = result.m[0][1]; // Symétrie
+    result.m[1][1] = mass * (d.x * d.x + d.z * d.z); 
+    result.m[1][2] = -mass * d.y * d.z;
+
+    result.m[2][0] = result.m[0][2]; // Symétrie
+    result.m[2][1] = result.m[1][2]; // Symétrie
+    result.m[2][2] = mass * (d.x * d.x + d.y * d.y); 
+
     return result;
 }
 
 struct rocket_parameters { // still sketching 
     //shell parameters
-    float mass_shell;
+    float mass_shell;   
     float mass_head;
     float density_shell;
     float radius_ext;
@@ -252,35 +270,38 @@ struct rocket_parameters { // still sketching
     float height;
     float head_height;
     float radius_int = radius_ext - thickness;
-    float pos_shell;
-    float pos_body;
-    float pos_head;
-    float Cog_shell = pos_shell + (height) / 2.0f; // assuming the shape is a perfect cylindre , a thing to change 
-    float CoP;
+    V3 pos_shell;
+    V3 pos_body;
+    V3 pos_head;
+    V3 Cog_shell = pos_shell + V3(0,0,height) / 2.0f; // assuming the shape is a perfect cylindre , a thing to change 
+    V3 CoG_head = V3(0,0,head_height / 4.0f);
     // motor parameters
     float radius_motor;
     float motor_length;
-    float mass_motor;
+    float mass_emp_motor;
+    float mass_fuel;
+    float mass_motor() const {return mass_emp_motor + mass_fuel;}
     float dm; // mass flow rate of the fuel, this one too
-    float pos_motor;
-    float CoG_motor = pos_motor + motor_length / 2.0f; // the cog is in the middle since the fuel + motor is assumed to be homogeneous, but later on when we will rework the motor parameters we will change it
+    V3 pos_motor;
+    V3 CoG_motor() const {return pos_motor + V3(0,0,motor_length) / 2.0f;} // the cog is in the middle since the fuel + motor is assumed to be homogeneous, but later on when we will rework the motor parameters we will change it
     // FC parameters
     float radius_FC = radius_int; // for now we will assume that the FC has the same radius as the inner radius of the shell, but we can change it later 
     float mass_FC;
     float length_FC;
-    float pos_FC;
-    float CoG_FC = pos_FC + length_FC / 2.0f; // we will assume that the FC is homogeneous
+    V3 pos_FC;
+    V3 CoG_FC = pos_FC + V3(0,0,length_FC) / 2.0f; // we will assume that the FC is homogeneous
     // nanosat parameters
     float length_nanosat;
     float width_nanosat;
     float height_nanosat;
     float mass_nanosat;
-    float pos_nanosat;
-    float CoG_nanosat = pos_nanosat + length_nanosat / 2.0f; // we will assume that the nanosat is homogeneous and that it is placed at the end of the rocket
+    V3 pos_nanosat;
+    V3 CoG_nanosat = pos_nanosat + V3(0,0,length_nanosat) / 2.0f; // we will assume that the nanosat is homogeneous and that it is placed at the end of the rocket
    
     float Cd; // drag coeff
     float Rho; // air density
-
+    float g; // gravity
+    V3 CoP;
 };
 
 class rocket_state { // still sketching
@@ -301,13 +322,12 @@ class rocket_state { // still sketching
     float dm; // mass flow rate of the fuel
 
     float get_mass_total() const {
-        return p.mass_shell + mass_fuel + p.mass_motor + p.mass_FC + p.mass_nanosat;
+        return p.mass_shell + p.mass_motor() + p.mass_FC + p.mass_nanosat;
     }
 
-    float get_CoG() const { // this is a function which calculates the CoG with the Huygens Steiner theorem
-        return (p.mass_shell * p.Cog_shell + p.mass_motor * p.CoG_motor 
-               + p.mass_FC * p.CoG_FC + p.mass_nanosat * p.CoG_nanosat 
-               + mass_fuel * p.CoG_motor) / get_mass_total();
+    V3 get_CoG() const { // this is a function which calculates the CoG with the Huygens Steiner theorem
+        return V3( p.Cog_shell * p.mass_shell  + p.CoG_motor() *  p.mass_motor()
+               + p.CoG_FC * p.mass_FC  + p.CoG_nanosat * p.mass_nanosat ) / get_mass_total();
     }
  // we will assume that the fuel is distributed uniformly along the motor length
     
@@ -315,36 +335,38 @@ class rocket_state { // still sketching
     rocket_state() : position(), velocity(), orientation(), angular_velocity(), mass_fuel(0.0f), dm(0.0f) {}
 
     M3 get_inertia_tensor() const { 
+        V3 CoG = get_CoG();
         
-        return inertia_cyclindre(p.mass_shell - p.mass_head, p.radius_ext, p.height-p.head_height)  +steiner(p.mass_shell-p.mass_head,(p.pos_body- get_CoG())) 
+        return inertia_cyclindre_emp(p.mass_shell - p.mass_head, p.radius_ext, p.radius_int, p.height-p.head_height)  +steiner(p.mass_shell-p.mass_head,(p.Cog_shell- CoG)) // body shell 
 
-        + inertia_rhead(p.density_shell, p.radius_ext, p.head_height) +steiner(p.mass_head, p.pos_head- get_CoG())
+        + inertia_rhead(p.density_shell, p.radius_ext, p.head_height) +steiner(p.mass_head, p.CoG_head- CoG) // head shell
 
-        + inertia_cyclindre(p.mass_FC, p.radius_FC*2, p.length_FC) +steiner(p.mass_FC,(p.pos_FC-get_CoG())) 
+        + inertia_cyclindre(p.mass_FC, p.radius_FC*2, p.length_FC) +steiner(p.mass_FC,(p.CoG_FC-CoG))  // FC
 
-        + inertia_parallelepiped(p.mass_nanosat, p.length_nanosat, p.width_nanosat, p.height_nanosat) +steiner(p.mass_nanosat,(p.CoG_nanosat - get_CoG()))
+        + inertia_parallelepiped(p.mass_nanosat, p.length_nanosat, p.width_nanosat, p.height_nanosat) +steiner(p.mass_nanosat,(p.CoG_nanosat - CoG)) // nanosat
         
-        + inertia_cyclindre(p.mass_motor,p.radius_motor,p.motor_length) +steiner(p.mass_motor, p.pos_motor- get_CoG());
+        + inertia_cyclindre(p.mass_motor() ,p.radius_motor,p.motor_length) +steiner(p.mass_motor(), p.CoG_motor() - CoG); // motor
     }
+
+    V3 get_d_drag() const { return p.CoP -  get_CoG();}
 
     V3 angular_accel(V3 angular_vel) const {
         V3 drag_body =orientation.rotation().transpose() * drag;  // in the body frame      
-        V3 d_thr = V3(0.0f, 0.0f, -p.pos_motor + get_CoG());
-        V3 d_drag = V3(0,0,-p.CoP + get_CoG());
+        V3 d_thr =p.pos_motor  - get_CoG() ;
+        V3 d_drag = get_d_drag();
         M3 I = get_inertia_tensor();
         M3 I_inv = I.inverse();
-        V3 torque = thrust.crossV_V(d_thr) + drag_body.crossV_V(d_drag);
+        V3 torque = d_thr.crossV_V(thrust) + d_drag.crossV_V(drag_body);
         return I_inv * (torque - angular_vel.crossV_V(I * angular_vel));
 }   
     V3 accel(V3 velocity) const {
-        V3 P = V3(0,0,-get_mass_total() * 9.81);
-        float A = p.radius_ext * p.radius_ext * PI;
+        V3 P = V3(0,0,-get_mass_total() * p.g);
+        float A = p.radius_ext * p.radius_ext * M_PI;
         V3 drag =  velocity * velocity.magnitude() * p.Rho * p.Cd * A *-0.5f ;
         V3 thrust_inertial = orientation.rotation() * thrust; // in the inertial frame
         V3 accel = ( drag + thrust_inertial + P) / get_mass_total();
-        return accel ; // mes vecteurs drag et thrust sont inertiel non translationnel faudrait les multiplier par des matrices de rotations inverses 
+        return accel ; 
     }
-    
 };
 
 class Rk4 {
@@ -356,6 +378,7 @@ class Rk4 {
         V3 k4 = s.angular_accel(s.angular_velocity + k3 * dt);
         s.angular_velocity = s.angular_velocity + (k1 + k2 * 2.0f + k3 * 2.0f + k4) * (dt / 6.0f); 
     }
+
     static void rk4(quaternion& q,rocket_state& s, float dt) {
         quaternion k1 = q.deriv(s.angular_velocity);
         quaternion k2 = (q + k1 * dt / 2.0f).deriv(s.angular_velocity);
@@ -370,39 +393,46 @@ class Rk4 {
         V3 kv2 = s.accel(s.velocity + kv1 * dt / 2.0f);
         V3 kv3 = s.accel(s.velocity + kv2 * dt / 2.0f);
         V3 kv4 = s.accel(s.velocity + kv3 * dt);
-        s.velocity = s.velocity + (kv1 + kv2 * 2.0f + kv3 * 2.0f + kv4) * (dt / 6.0f);
         
         V3 kp1 = s.velocity;
         V3 kp2 = s.velocity + kp1 * dt/2.0f;
         V3 kp3 = s.velocity + kp2 * dt/2.0f;
         V3 kp4 = s.velocity + kp3 * dt;
+
+        s.velocity = s.velocity + (kv1 + kv2 * 2.0f + kv3 * 2.0f + kv4) * (dt / 6.0f);
         s.position = s.position + (kp1 + kp2*2 + kp3*2 + kp4) * (dt/6.0f);
     }
 };
 
+int main() {
+    for(;;) {
+
+    }
+}
+
     /* 
     *Done :
-    -now we have RK4 integrator for the rotation and translational, I think that it is not coupled tho and the
-    errors are going to persist after some reflexion, we will rework the RK4 class to unify everything 
+    -now we have RK4 integrator for the rotation and translational
     -reworked all the algebra tools and some mathematical tools 
     -reworked the quaternion class to be more efficient and more accurate
     -finally finished with inertia tensors and parameters structure even tho we're still missing some parameters 
     -moddeled the the physics
     
     -Priorities :
-    *rework the RK4 method
     *model the atmosphere following the ISA 
     *model the thrust engine
     *execute the loop in the file 
 
+
     -notes :
-    *we're assuming the rocket is symetric in the xy plane
-    *we're still treating some variating objects as constants
+    *we're still treating some variating objects as constants such as the center of pressure 
     *we don't have the drag coefficient graph yet so we assume it to be a constant
-    *the model will get more accurate later on even when we're not assuming the rocket is 
+    *the model got more accurate since we started not assuming everything to be symmetrical
+    *the file will be seperated into many files to ease the maintenance of the code/program
     
     */
-    /* Last updated : 2026-06-14 */
+   
+    /* Last updated : 2026-06-18 */
     
 
 
